99eae2a92f4b1a541cf99c8a2fea35ab94b86b13
PedroWeb10 <pdro_dev@yahoo.com>
2025-01-18 10:03:52 -0300

    solution transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,35 @@
 'use strict';
 
 /**
- * @param {Object} state
+ * @param {Object} state //
  * @param {Object[]} actions
  *
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
   // write code here
-}
+  const stateResult = [];
+  let matrizObj = { ...state };
 
+  actions.forEach((element) => {
+    switch (element.type) {
+      case 'addProperties':
+        matrizObj = { ...matrizObj, ...element.extraData };
+        break;
+      case 'removeProperties':
+        matrizObj = { ...matrizObj };
+
+        element.keysToRemove.forEach((key) => {
+          delete matrizObj[key];
+        });
+        break;
+      case 'clear':
+        matrizObj = {};
+        break;
+    }
+    stateResult.push({ ...matrizObj });
+  });
+
+  return stateResult;
+}
 module.exports = transformStateWithClones;
